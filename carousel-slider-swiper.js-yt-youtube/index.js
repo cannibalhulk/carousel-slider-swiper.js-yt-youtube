e797d75e0a2d6b9ad9b93909409ff7d017f72f5a
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2022-09-13 00:27:41 +0400

    changes

--- carousel-slider-swiper.js-yt-youtube/index.js
+++ carousel-slider-swiper.js-yt-youtube/index.js
@@ -12,16 +12,16 @@ const swiper = new Swiper('.swiper', {
 	keyboard: true,
 	mousewheel: true,
 
-	// effect: 'creative',
-	// creativeEffect: {
-	// 	prev: {
-	// 		// will set `translateZ(-400px)` on previous slides
-	// 		translate: [0, 0, -400],
-	// 	},
-	// 	next: {
-	// 		// will set `translateX(100%)` on next slides
-	// 		translate: ['100%', 0, 0],
-	// 	},
-	// },
+	effect: 'creative',
+	creativeEffect: {
+		prev: {
+			// will set `translateZ(-400px)` on previous slides
+			translate: [0, 0, -400],
+		},
+		next: {
+			// will set `translateX(100%)` on next slides
+			translate: ['100%', 0, 0],
+		},
+	},
 	// effect: 'coverflow',
 })
